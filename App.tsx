cf865230add3cab0c3dc5504c0733c017f6f5ef3
Livol <liv.olsen@globetracker.com>
2022-11-27 21:16:11 +0100

    Added nativebaseprovider

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {TabNavigator} from './src/navigation/tabNavigator';
 import {Dark} from './src/styles/navigationBar';
+import {NativeBaseProvider} from 'native-base';
 
 export default function App() {
   return (
-    <NavigationContainer theme={Dark}>
-      <TabNavigator />
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer theme={Dark}>
+        <TabNavigator />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
