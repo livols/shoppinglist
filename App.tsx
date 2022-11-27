090a27fdc3076c4a805d99eb235a4a20ccaee069
Livol <liv.olsen@globetracker.com>
2022-11-27 15:57:26 +0100

    Relocated pages

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Home} from './pages/home';
-import {Recipe} from './pages/recipe';
-import {Settings} from './pages/settings';
-import {Notification} from './pages/notification';
-
-const Tab = createBottomTabNavigator();
+import {TabNavigator} from './src/navigation/tabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Recipe" component={Recipe} />
-        <Tab.Screen name="Settings" component={Settings} />
-        <Tab.Screen name="Notification" component={Notification} />
-      </Tab.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
