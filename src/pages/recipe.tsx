fd5d51b9b5c20cd7300236ce855572f426bf8d0f
Livol <liv.olsen@globetracker.com>
2022-11-27 22:23:33 +0100

    Added colors, input and button with style

--- src/pages/recipe.tsx
+++ src/pages/recipe.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from '../styles/colors';
+import {Button, Input, Stack} from 'native-base';
 
 export function Recipe() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Recipe!</Text>
+    <View>
+      <View style={{paddingLeft: 23, paddingTop: 15}}>
+        <Text style={styles.text}>Add new recipe</Text>
+      </View>
+      <View style={{paddingLeft: 23, paddingTop: 12}}>
+        <Stack direction="row" mb="2.5" mt="1.5" space={2}>
+          <Input
+            type="text"
+            placeholder="Enter URL with recipe"
+            w="299"
+            h="45"
+            backgroundColor={Colors.BAR}
+            borderRadius="10"
+            borderColor={Colors.BAR_BORDER}
+            placeholderTextColor={Colors.PLACEHOLDER}
+            color={Colors.TEXT}
+            _focus={{
+              borderColor: Colors.BAR_BORDER,
+            }}
+          />
+          <Button
+            backgroundColor={Colors.BUTTON}
+            borderColor={Colors.BUTTON_BORDER}
+            borderRadius={10}>
+            HIT
+          </Button>
+        </Stack>
+      </View>
+      <View style={{paddingLeft: 18, paddingTop: 15}}>
+        <Text style={styles.text}>Recipes</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: Colors.TEXT,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,7 +3,9 @@ export const Colors = {
   BUTTON_BORDER: '#14985C',
   BACKGROUND: '#13131A',
   BAR: '#1C1C24',
+  BAR_BORDER: '#2B2B38',
   TEXT: '#FFFFFF',
   INACTIVE: '#818181',
   NOTIFICATION: 'rgb(255, 69, 58)',
+  PLACEHOLDER: '#4C4C63',
 };
